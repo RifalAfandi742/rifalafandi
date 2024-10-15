92fc2581dcfa6cae22a873d27bdc5c32a391d3dd
Rifal Afandi <rifallighter@gmail.com>
2024-10-15 19:53:36 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// Toggle class active untuk humberger menu
+
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika humburger menu diclik
+
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Toggle class active untuk search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle("active");
+  searchBox.focus();
+  e.preventDefault();
+}
+
+// klik diluar elemen
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
+
+document.addEventListener('click', function(e){
+    if(!hm.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove("active");
+    }
+
+    if(!sb.contains(e.target) && !searchForm.contains(e.target)){
+        searchForm.classList.remove("active");
+    }
+})
+
+
